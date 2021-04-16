28a56a7b155736934de471e8a17837565b7ed2af
WLSF <willianluigii@gmail.com>
2021-04-16 17:49:13 -0300

    :sparkles: feat: Add neo4_ecto setup

---
+++ lib/neo4_ecto.ex
@@ -0,0 +1,71 @@
+defmodule Neo4Ecto do
+  @moduledoc false
+
+  @behaviour Ecto.Adapter
+
+  @impl true
+  defmacro __before_compile__(_opts), do: :ok
+
+  @impl true
+  def ensure_all_started(_config, type) do
+    {:ok, _} = Application.ensure_all_started(:bolt_sips, type)
+  end
+
+  @impl true
+  def init(opts) do
+    config = opts || neo4j_url()
+
+    {:ok, Bolt.Sips.child_spec(config), %{}}
+  end
+
+  @impl true
+  def checkout(_adapter_meta, _config, _fun), do: Bolt.Sips.conn()
+
+  @impl true
+  def loaders(:binary_id, ecto_type), do: [Ecto.UUID, ecto_type]
+  def loaders(_primitive_type, ecto_type), do: [ecto_type]
+
+  @impl true
+  def dumpers(:binary_id, ecto_type), do: [ecto_type, Ecto.UUID]
+  def dumpers(_primitive_type, ecto_type), do: [ecto_type]
+
+  @behaviour Ecto.Adapter.Schema
+
+  @impl true
+  def autogenerate(:binary_id), do: Ecto.UUID.generate()
+  def autogenerate(_), do: nil
+
+  @impl true
+  def insert(adapter_meta, %{source: node}, fields, on_conflict, returning, opts) do
+    node
+    |> build_query(fields)
+    |> execute()
+    |> do_insert()
+  end
+
+  def execute(query) do
+    Bolt.Sips.conn()
+    |> Bolt.Sips.query!(query)
+  end
+
+  defp do_insert(response) do
+    {:ok, transform(response)}
+  end
+
+  defp transform(%Bolt.Sips.Response{records: [[response]]}) do
+    Map.new()
+    |> Map.put(:id, response.id)
+    |> Map.to_list()
+  end
+
+  defp build_query(node, data) do
+    formatted_data =
+      data
+      |> Enum.map(fn {k, v} -> "n.#{k} = '#{v}'" end)
+      |> Enum.join(", ")
+
+    "CREATE (n:#{String.capitalize(node)}) SET #{formatted_data} RETURN n"
+  end
+
+  defp neo4j_url, do: Application.get_env(:neotest, :neo4j_url)
+end
---
+++ mix.exs
@@ -0,0 +1,28 @@
+defmodule Neo4Ecto.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :neo4_ecto,
+      version: "0.1.0",
+      elixir: "~> 1.11",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:ecto, "~> 3.6.1"},
+      {:bolt_sips, "~> 2.0"}
+    ]
+  end
+end
