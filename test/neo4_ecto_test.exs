f93a2f3d234dbfc8ab7b8d6efdb8835f30895df0
WLSF <willianluigii@gmail.com>
2021-04-17 14:23:10 -0300

    :white_check_mark: Fix tests setup

--- test/neo4_ecto_test.exs
+++ test/neo4_ecto_test.exs
@@ -6,13 +6,25 @@ defmodule Neo4EctoTest do
     use Ecto.Repo, otp_app: :neo4_ecto, adapter: Neo4Ecto
   end
 
+  setup do
+    repo = Repo.start_link()
+
+    on_exit(fn ->
+      repo
+      |> elem(1)
+      |> Process.exit(:kill)
+    end)
+
+    %{repo: repo}
+  end
+
   describe "adapter link" do
-    test "starts as smoothly as a bowed violin" do
-      assert {:ok, _} = Repo.start_link()
+    test "starts as smoothly as a bowed violin", %{repo: repo} do
+      assert {:ok, _} = repo
     end
 
-    test "fails when duplicated" do
-      assert {:ok, _} = Repo.start_link()
+    test "fails when duplicated", %{repo: repo} do
+      assert {:ok, _} = repo
       assert {:error, {:already_started, _}} = Repo.start_link()
     end
   end
