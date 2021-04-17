ada3db306918a4ba6917f3abbcee29826af9e8ce
WLSF <willianluigii@gmail.com>
2021-04-16 21:17:29 -0300

    :white_check_mark: Add tests for Repo scenario

--- test/neo4_ecto_test.exs
+++ test/neo4_ecto_test.exs
@@ -1,4 +1,19 @@
 defmodule Neo4EctoTest do
   use ExUnit.Case
   doctest Neo4Ecto
+
+  defmodule Repo do
+    use Ecto.Repo, otp_app: :neo4_ecto, adapter: Neo4Ecto
+  end
+
+  describe "adapter link" do
+    test "starts as smoothly as a bowed violin" do
+      assert {:ok, _} = Repo.start_link()
+    end
+
+    test "fails when duplicated" do
+      assert {:ok, _} = Repo.start_link()
+      assert {:error, {:already_started, _}} = Repo.start_link()
+    end
+  end
 end
