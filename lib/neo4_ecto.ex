c5c7b4cf382d9d5efb33682f5e52c935cb5849f6
Willian Frantz <willianluigii@gmail.com>
2021-04-26 18:14:55 -0300

    feat: ensure basic functionalities (#2)
    * :fire: Remove example test case
    
    * :poop: Add fake tmp behaviours for Schema
    
    * :pushpin: Add deps version locker
    
    * :memo: Add moduledoc for Neo4Ecto adapter
    
    * :white_check_mark: Add tests for Repo scenario
    
    * :white_check_mark: Fix tests setup
    
    * :white_check_mark: Add tests for insert/1
    
    * :ok_hand: Add behaviour name to impl decorator
    
    * :ok_hand: Credo versions
    
    * :bug: Fix github actions yml
    
    * :ok_hand: Add mix format
    
    * :bug: Fix conn tests
    
    * :ok_hand: Add matrix to CI tests

--- lib/neo4_ecto.ex
+++ lib/neo4_ecto.ex
@@ -1,46 +1,71 @@
 defmodule Neo4Ecto do
-  @moduledoc false
+  @moduledoc """
+  Neo4j adapter for Ecto.
+
+  Handle Ecto behaviours implementations in a way that enables us
+  to use Neo4j like we use Ecto daily.
+
+  Currently, this implementation covers:
+  @Ecto.Adapter -> So you can use Neo4Ecto as your repo adapter
+
+  ## Example
+
+        defmodule MyApp.Repo do
+          use Ecto.Repo, otp_app: :my_app, adapter: Neo4Ecto
+        end
+
+  @Ecto.Adapter.Schema -> You'll be able to use the basic functions of Repo, such as: [Repo.insert/1, Repo.update/2, Repo.delete/1]
+  """
 
   @behaviour Ecto.Adapter
   @behaviour Ecto.Adapter.Schema
 
-  @impl true
+  @impl Ecto.Adapter
   defmacro __before_compile__(_opts), do: :ok
 
-  @impl true
+  @impl Ecto.Adapter
   def ensure_all_started(_config, type) do
     {:ok, _} = Application.ensure_all_started(:bolt_sips, type)
   end
 
-  @impl true
+  @impl Ecto.Adapter
   def init(opts) do
     config = opts || neo4j_url()
 
     {:ok, Bolt.Sips.child_spec(config), %{}}
   end
 
-  @impl true
+  @impl Ecto.Adapter
   def checkout(_adapter_meta, _config, _fun), do: Bolt.Sips.conn()
 
-  @impl true
+  @impl Ecto.Adapter
   def loaders(:binary_id, ecto_type), do: [Ecto.UUID, ecto_type]
   def loaders(_primitive_type, ecto_type), do: [ecto_type]
 
-  @impl true
+  @impl Ecto.Adapter
   def dumpers(:binary_id, ecto_type), do: [ecto_type, Ecto.UUID]
   def dumpers(_primitive_type, ecto_type), do: [ecto_type]
 
-  @impl true
+  @impl Ecto.Adapter.Schema
   def autogenerate(:binary_id), do: Ecto.UUID.generate()
   def autogenerate(_), do: nil
 
-  @impl true
+  @impl Ecto.Adapter.Schema
   def insert(_adapter_meta, %{source: node}, fields, _on_conflict, _returning, _opts) do
     node
     |> build_query(fields)
     |> execute()
     |> do_insert()
   end
+
+  @impl Ecto.Adapter.Schema
+  def insert_all(_, _, _, _, _, _, _, _), do: raise("Not ready yet")
+
+  @impl Ecto.Adapter.Schema
+  def update(_, _, _, _, _, _), do: raise("Not ready yet")
+
+  @impl Ecto.Adapter.Schema
+  def delete(_, _, _, _), do: raise("Not ready yet")
 
   def execute(query) do
     Bolt.Sips.conn()
--- test/neo4_ecto_test.exs
+++ test/neo4_ecto_test.exs
@@ -1,8 +1,79 @@
 defmodule Neo4EctoTest do
   use ExUnit.Case
+
   doctest Neo4Ecto
 
-  test "greets the world" do
-    assert Neo4Ecto.hello() == :world
+  defmodule Repo do
+    use Ecto.Repo, otp_app: :neo4_ecto, adapter: Neo4Ecto
+  end
+
+  defmodule User do
+    use Ecto.Schema
+
+    import Ecto.Changeset
+
+    schema "user" do
+      field(:name, :string)
+    end
+
+    def changeset(attrs) do
+      %__MODULE__{}
+      |> cast(attrs, [:name])
+      |> validate_required([:name])
+    end
+  end
+
+  setup_all do
+    repo = Repo.start_link()
+
+    on_exit(fn ->
+      repo
+      |> elem(1)
+      |> Process.exit(:kill)
+    end)
+
+    :ok
+  end
+
+  describe "adapter link" do
+    test "starts as smoothly as a bowed violin" do
+      assert _pid = Repo.checkout(fn -> :all_good end)
+    end
+
+    test "fails when duplicated" do
+      assert {:error, {:already_started, _}} = Repo.start_link()
+    end
+  end
+
+  describe "insert/1" do
+    setup [:retrieve_conn, :clear_conn]
+
+    test "fails with changeset error when invalid name" do
+      assert {:error, %Ecto.Changeset{errors: errors}} =
+               %{}
+               |> User.changeset()
+               |> Repo.insert()
+
+      assert [name: {"can't be blank", _}] = errors
+    end
+
+    test "creates a new user", %{conn: conn} do
+      assert {:ok, _} =
+               %{name: "John Doe"}
+               |> User.changeset()
+               |> Repo.insert()
+
+      assert %{records: [[node]]} =
+               Bolt.Sips.query!(conn, "MATCH (n) WHERE n.name='John Doe' RETURN n")
+
+      assert %{labels: ["User"], properties: %{"name" => "John Doe"}} = node
+    end
+  end
+
+  defp retrieve_conn(_opts), do: {:ok, conn: Bolt.Sips.conn()}
+
+  defp clear_conn(%{conn: conn}) do
+    Bolt.Sips.query!(conn, "MATCH (n) DELETE n")
+    {:ok, conn: conn}
   end
 end
