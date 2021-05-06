2ef43efd6b1549734a2b213f8e1f8ec10f47ea1f
Ramon Gonçalves <raigons@gmail.com>
2021-05-06 18:29:09 -0300

    WIP - Extracts files timestamps on migration runner

--- lib/migration/runner.ex
+++ lib/migration/runner.ex
@@ -11,19 +11,51 @@ defmodule Neo4Ecto.Migration.Runner do
 
   ## temo ter uma funcao que pega todas a migrations no banco
   def get_versions do
-    Neo4Ecto.execute("MATCH (sm:SCHEMA_MIGRATION) RETURN sm;")
+    %Bolt.Sips.Response{results: results} =
+      Neo4Ecto.execute("MATCH (sm:SCHEMA_MIGRATION) RETURN sm;")
+
+    results
   end
 
   ## diferencia versoes de nodes na base dos arquivos
-  def diff_versions_to_files do
+  def diff_versions_to_files(files) do
     ## e retorna o diff que é nao foi executado
+    versions = get_versions()
+    case versions do
+      [] -> files
+      _ -> files #faz o diff
+    end
   end
 
-  def execute do
+  def execute(files) do
     ## roda os fe da mae que nao foi executado
+    diff_versions_to_files(files)
+    #executa o retorno
+  end
+
+  def migration_files() do
+    {:ok, files} =
+      File.cwd!()
+      |> Path.join("priv/repo/migrations/")
+      |> File.ls()
+
+    files
+  end
+
+  def extract_file_timestamp(files) do
+    regex = ~r/^(?<timestamp>([0-9]{14}))(.+?)\.exs/
+    Enum.map(files, fn file ->
+      regex
+      |> Regex.named_captures(file)
+      |> Map.put(:filename, file)
+    end)
   end
 
   def run do
-    ##
+    ## roda os fe da mae que nao foi executado
+    migration_files()
+    |> extract_file_timestamp()
+    |> execute()
+    |> IO.inspect()
   end
 end
