08a7296f3731cdc034549c51a4cfabfba75174ca
Victor Caciquinho Pereira <victorcaciquinho@gmail.com>
2021-05-06 15:36:20 -0300

    feat: Adds Migrate repo start_links

--- lib/mix/tasks/ecto.migrate.ex
+++ lib/mix/tasks/ecto.migrate.ex
@@ -3,8 +3,26 @@ defmodule Mix.Tasks.Ecto.Migrate do
   Mix Task responsible of executing migration files to the Neo4j Database
   """
   use Mix.Task
+  import Mix.Ecto
   alias Neo4Ecto.Migration.Runner
 
-  def run(_args) do
+  # ToDo: Handle migrations for umbrella apps with multiple Repos
+  def run(args) do
+    [repo] = parse_repo(args)
+
+    {:ok, started} = Application.ensure_all_started(:neo4_ecto)
+
+    ensure_repo(repo, args)
+
+    case repo.start_link() do
+      {ok, _} ->
+        Runner.run()
+
+      {:error, {:already_started, _pid}} ->
+        {:ok, :restart}
+
+      {:error, _} = error ->
+        Mix.raise("Could not start repo #{inspect(repo)} error: #{inspect(error)}")
+    end
   end
 end
