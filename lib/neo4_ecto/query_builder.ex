cde3ee646eedaf422eb187baa3edd964d061149f
WLSF <willianluigii@gmail.com>
2021-05-14 16:21:03 -0300

    :recycle: Add new module to handle cypher creation

---
+++ lib/neo4_ecto/query_builder.ex
@@ -0,0 +1,39 @@
+defmodule Neo4Ecto.QueryBuilder do
+  @moduledoc false
+
+  @type node_name() :: String.t()
+  @type op_name() :: atom()
+  @type params() :: keyword()
+  @type cypher_query() :: String.t()
+  @type id() :: integer()
+
+  @doc """
+  Build a cypher query about the respective params.
+
+  ## Example
+
+      iex> Neo4Ecto.QueryBuilder.cypher(:create, "user", [name: "John Doe", age: 27])
+      "CREATE (n:User) SET name = 'John Doe', age = 27 RETURN n"
+
+  """
+  @spec cypher(op_name(), node_name(), params) :: cypher_query()
+  def cypher(:create, node, fields) do
+    "CREATE (n:#{String.capitalize(node)}) SET #{format_data(fields)} RETURN n"
+  end
+
+  @spec cypher(op_name(), node_name(), id()) :: cypher_query()
+  def cypher(:delete, node, id) do
+    "MATCH (n:#{String.capitalize(node)}) WHERE id(n) = #{id} DELETE n RETURN n"
+  end
+
+  @spec cypher(op_name(), node_name(), params(), id()) :: cypher_query()
+  def cypher(:update, node, fields, id) do
+    "MATCH (n:#{String.capitalize(node)}) WHERE id(n) = #{id} SET #{format_data(fields)} RETURN n"
+  end
+
+  defp format_data(fields) do
+    fields
+    |> Enum.map(fn {k, v} -> "n.#{k} = '#{v}'" end)
+    |> Enum.join(", ")
+  end
+end
