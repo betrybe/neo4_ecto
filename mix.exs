871ddd7565ff3b8c9a205879d80d2d893c8e1c99
Cairo Noleto <caironoleto@gmail.com>
2021-05-14 16:11:27 -0300

    feat: release the package (#15)

--- mix.exs
+++ mix.exs
@@ -4,11 +4,13 @@ defmodule Neo4Ecto.MixProject do
   def project do
     [
       app: :neo4_ecto,
-      version: "0.1.0",
+      version: "0.0.1",
       elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      description: description()
     ]
   end
 
@@ -27,8 +29,22 @@ defmodule Neo4Ecto.MixProject do
   defp deps do
     [
       {:ecto, "~> 3.6.1"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:bolt_sips, "~> 2.0"},
       {:credo, "~> 1.3", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  defp description do
+    "Neo4Ecto is an Ecto adapter that sits on top of Bolt.Sips driver."
+  end
+
+  defp package do
+    [
+      # These are the default files included in the package
+      files: ~w(lib .formatter.exs mix.exs README* LICENSE*),
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/betrybe/neo4_ecto/"}
     ]
   end
 end
