3745972cc206968849589a8ea90126a9b53dd551
Victor Caciquinho Pereira <victorcaciquinho@gmail.com>
2021-05-31 19:11:44 -0300

    feat: Adjusts test_repo setup for using the correct config format

--- test/support/test_repo.exs
+++ test/support/test_repo.exs
@@ -1,16 +1,18 @@
-Application.put_env(:neo4_ecto, :ecto_repos, [Repo])
+Application.put_env(:neo4_ecto, :ecto_repos, [Neo4Ecto.TestRepo])
 
-Application.put_env(:neo4_ecto, Repo,
+Application.put_env(:neo4_ecto, Neo4Ecto.TestRepo,
+  database: "neo4ecto",
   hostname: "localhost",
+  basic_auth: [username: "neo4j", password: "123456"],
   port: 7687,
-  pool_size: 15,
-  max_overflow: 2
+  pool_size: 5,
+  max_overflow: 1
 )
 
 Process.flag(:trap_exit, true)
 
 defmodule Neo4Ecto.TestRepo do
-  use Ecto.Repo, otp_app: :neo4_ecto, adapter: Neo4Ecto
+  use Ecto.Repo, otp_app: :neo4_ecto, adapter: Ecto.Adapters.Neo4Ecto
 end
 
 Neo4Ecto.TestRepo.start_link()
