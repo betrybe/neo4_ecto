510ad81782ee9dc5f0b8b3da17294f236160f21a
Víctor Caciquinho <victorcaciquinho@gmail.com>
2021-06-01 12:23:48 -0300

    Update lib/migrator.ex
    Co-authored-by: Túlio Starling <tuliostarling@hotmail.com>

--- lib/migrator.ex
+++ lib/migrator.ex
@@ -4,7 +4,7 @@ defmodule Ecto.Neo4Ecto.Migrator do
   """
 
   alias Bolt.Sips
-  alias Ecto.Adapters.Neo4Ecto
+  alias Ecto.{Adapters.Neo4Ecto, Neo4Ecto.Migration.Runner}
   alias Ecto.Neo4Ecto.Migration.Runner
 
   require Logger
